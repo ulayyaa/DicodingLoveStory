10b53079a9e4d9f093c61caad7db3dde5d48c446
ulayya <ulayyaathifal@gmail.com>
2025-05-26 17:20:06 +0700

    Initial commit

---
+++ src/scripts/pages/new/new-presenter.js
@@ -0,0 +1,47 @@
+export default class NewPresenter {
+  #view;
+  #model;
+
+  constructor({ view, model }) {
+    this.#view = view;
+    this.#model = model;
+  }
+
+  async showNewFormMap() {
+    this.#view.showMapLoading();
+    try {
+      await this.#view.initialMap();
+    } catch (error) {
+      console.error('showNewFormMap: error:', error);
+    } finally {
+      this.#view.hideMapLoading();
+    }
+  }
+
+  async postNewStory({ title, description, photoUrl, latitude, longitude }) {
+    this.#view.showSubmitLoadingButton();
+    try {
+      const data = {
+        title: title,
+        description: description,
+        photoUrl: photoUrl,
+        latitude: latitude,
+        longitude: longitude,
+      };
+      const response = await this.#model.storeNewStory(data);
+
+      if (!response.ok) {
+        console.error('postNewStory: response:', response);
+        this.#view.storeFailed(response.message);
+        return;
+      }
+
+      this.#view.storeSuccessfully(response.message, response.data);
+    } catch (error) {
+      console.error('postNewStory: error:', error);
+      this.#view.storeFailed(error.message);
+    } finally {
+      this.#view.hideSubmitLoadingButton();
+    }
+  }
+}
---
+++ src/scripts/templates.js
@@ -0,0 +1,256 @@
+import { showFormattedDate } from './utils';
+
+export function generateLoaderTemplate() {
+  return `
+    <div class="loader"></div>
+  `;
+}
+
+export function generateLoaderAbsoluteTemplate() {
+  return `
+    <div class="loader loader-absolute"></div>
+  `;
+}
+
+export function generateMainNavigationListTemplate() {
+  return `
+    <li><a id="story-list-button" class="story-list-button" href="#/">Daftar Story</a></li>
+    <li><a id="bookmark-button" class="bookmark-button" href="#/bookmark">Story Tersimpan</a></li>
+  `;
+}
+
+export function generateUnauthenticatedNavigationListTemplate() {
+  return `
+    <li id="push-notification-tools" class="push-notification-tools"></li>
+    <li><a id="login-button" href="#/login">Login</a></li>
+    <li><a id="register-button" href="#/register">Register</a></li>
+  `;
+}
+
+export function generateAuthenticatedNavigationListTemplate() {
+  return `
+    <li id="push-notification-tools" class="push-notification-tools"></li>
+    <li><a id="new-story-button" class="btn new-story-button" href="#/new">Buat Laporan <i class="fas fa-plus"></i></a></li>
+    <li><a id="logout-button" class="logout-button" href="#/logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+  `;
+}
+
+export function generatestoriesListEmptyTemplate() {
+  return `
+    <div id="stories-list-empty" class="stories-list__empty">
+      <h2>Tidak ada laporan yang tersedia</h2>
+      <p>Saat ini, tidak ada laporan kerusakan fasilitas umum yang dapat ditampilkan.</p>
+    </div>
+  `;
+}
+
+export function generatestoriesListErrorTemplate(message) {
+  return `
+    <div id="stories-list-error" class="stories-list__error">
+      <h2>Terjadi kesalahan pengambilan daftar story</h2>
+      <p>${message ? message : 'Gunakan jaringan lain atau laporkan error ini.'}</p>
+    </div>
+  `;
+}
+
+export function generatestoryDetailErrorTemplate(message) {
+  return `
+    <div id="stories-detail-error" class="stories-detail__error">
+      <h2>Terjadi kesalahan pengambilan detail story</h2>
+      <p>${message ? message : 'Gunakan jaringan lain atau laporkan error ini.'}</p>
+    </div>
+  `;
+}
+
+export function generateCommentsListEmptyTemplate() {
+  return `
+    <div id="story-detail-comments-list-empty" class="story-detail__comments-list__empty">
+      <h2>Tidak ada komentar yang tersedia</h2>
+      <p>Saat ini, tidak ada komentar yang dapat ditampilkan.</p>
+    </div>
+  `;
+}
+
+export function generateCommentsListErrorTemplate(message) {
+  return `
+    <div id="story-detail-comments-list-error" class="story-detail__comments-list__error">
+      <h2>Terjadi kesalahan pengambilan daftar komentar</h2>
+      <p>${message ? message : 'Gunakan jaringan lain atau laporkan error ini.'}</p>
+    </div>
+  `;
+}
+
+export function generatestoryItemTemplate({
+  id,
+  name,
+  description,
+  photourl,
+  createdAt,
+  lat,
+  lon,
+}) {
+  return `
+    <div tabindex="0" class="story-item" data-storyid="${id}">
+      <img class="story-item__image" src="${photourl}" alt="${name}">
+      <div class="story-item__body">
+        <div class="story-item__main">
+          <h2 id="story-title" class="story-item__title">${name}</h2>
+          <div class="story-item__more-info">
+            <div class="story-item__createdat">
+              <i class="fas fa-calendar-alt"></i> ${showFormattedDate(createdAt, 'id-ID')}
+            </div>
+            <div class="story-item__location">
+              <i class="fas fa-map"></i> ${lat} - ${lon}
+            </div>
+          </div>
+        </div>
+        <div id="story-description" class="story-item__description">
+          ${description}
+        </div>
+        <div class="story-item__more-info">
+          <div class="story-item__author">
+            Dilaporkan oleh: ${name}
+          </div>
+        </div>
+        <a class="btn story-item__read-more" href="#/stories/${id}">
+          Selengkapnya <i class="fas fa-arrow-right"></i>
+        </a>
+      </div>
+    </div>
+  `;
+}
+
+export function generatestoryDetailImageTemplate(imageUrl = null, alt = '') {
+  if (!imageUrl) {
+    return `
+      <img class="story-detail__image" src="images/placeholder-image.jpg" alt="Placeholder Image">
+    `;
+  }
+
+  return `
+    <img class="story-detail__image" src="${imageUrl}" alt="${alt}">
+  `;
+}
+
+export function generatestoryCommentItemTemplate({ photoUrlCommenter, nameCommenter, body }) {
+  return `
+    <article tabindex="0" class="story-detail__comment-item">
+      <img
+        class="story-detail__comment-item__photo"
+        src="${photoUrlCommenter}"
+        alt="Commenter name: ${nameCommenter}"
+      >
+      <div class="story-detail__comment-item__body">
+        <div class="story-detail__comment-item__body__more-info">
+          <div class="story-detail__comment-item__body__author">${nameCommenter}</div>
+        </div>
+        <div class="story-detail__comment-item__body__text">${body}</div>
+      </div>
+    </article>
+  `;
+}
+
+export function generatestoryDetailTemplate({
+  name,
+  description,
+  photoUrl,
+  location,
+  createdAt,
+}) {
+  const createdAtFormatted = showFormattedDate(createdAt, 'id-ID');
+  const imagesHtml = photoUrl.reduce(
+    (accumulator, photoUrl) =>
+      accumulator.concat(generatestoryDetailImageTemplate(photoUrl, name)),
+    '',
+  );
+
+  return `
+    <div class="story-detail__header">
+      <h1 id="title" class="story-detail__title">${name}</h1>
+
+      <div class="story-detail__more-info">
+        <div class="story-detail__more-info__inline">
+          <div id="createdat" class="story-detail__createdat" data-value="${createdAtFormatted}"><i class="fas fa-calendar-alt"></i></div>
+          <div id="location-place-name" class="storydetail__location__place-name" data-value="${location.placeName}"><i class="fas fa-map"></i></div>
+        </div>
+        <div class="story-detail__more-info__inline">
+        <div id="location-latitude" class="story-detail__location__latitude" data-value="${location.latitude}">Latitude:</div>
+        <div id="location-longitude" class="storydetail__location__longitude" data-value="${location.longitude}">Longitude:</div>
+        <div id="location-latitude" class="storydetail__location__latitude" data-value="${latitudeLocation}">Latitude:</div>
+          <div id="location-longitude" class="storydetail__location__longitude" data-value="${longitudeLocation}">Longitude:</div>
+        </div>
+        <div id="author" class="story-detail__author" data-value="${name}}">Dilaporkan oleh:</div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="storydetail__images__container">
+        <div id="images" class="story-detail__images">${imagesHtml}</div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="story-detail__body">
+        <div class="story-detail__body__description__container">
+          <h2 class="story-detail__description__title">Informasi Lengkap</h2>
+          <div id="description" class="story-detail__description__body">
+            ${description}
+          </div>
+        </div>
+        <div class="story-detail__body__map__container">
+          <h2 class="story-detail__map__title">Peta Lokasi</h2>
+          <div class="story-detail__map__container">
+            <div id="map" class="story-detail__map"></div>
+            <div id="map-loading-container"></div>
+          </div>
+        </div>
+  
+        <hr>
+  
+        <div class="story-detail__body__actions__container">
+          <h2>Aksi</h2>
+          <div class="story-detail__actions__buttons">
+            <div id="save-actions-container"></div>
+            <div id="notify-me-actions-container">
+              <button id="story-detail-notify-me" class="btn btn-transparent">
+                Try Notify Me <i class="far fa-bell"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+export function generateSubscribeButtonTemplate() {
+  return `
+    <button id="subscribe-button" class="btn subscribe-button">
+      Subscribe <i class="fas fa-bell"></i>
+    </button>
+  `;
+}
+
+export function generateUnsubscribeButtonTemplate() {
+  return `
+    <button id="unsubscribe-button" class="btn unsubscribe-button">
+      Unsubscribe <i class="fas fa-bell-slash"></i>
+    </button>
+  `;
+}
+
+export function generateSavestoryButtonTemplate() {
+  return `
+    <button id="story-detail-save" class="btn btn-transparent">
+      Simpan laporan <i class="far fa-bookmark"></i>
+    </button>
+  `;
+}
+
+export function generateRemovestoryButtonTemplate() {
+  return `
+    <button id="story-detail-remove" class="btn btn-transparent">
+      Buang laporan <i class="fas fa-bookmark"></i>
+    </button>
+  `;
+}
